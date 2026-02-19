c9ed9ca0d4d60ff47629e148e48f18229e640f63
wenyu <232058504+vvenyu@users.noreply.github.com>
2026-02-19 19:58:44 +0100

    The screen is working

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function divide(a, b) {
     return a / b;
 };
 
-let number;
+let number = '';
 let operator;
 let secondNumber;
 
@@ -29,4 +29,22 @@ function operate(number, operator, secondNumber) {
     } else if (operator === 'divide') {
         return divide(number, secondNumber);
     } else return "ERROR"
-}
+};
+
+const screen = document.querySelector('.screen');
+const numbers = document.querySelectorAll('.number');
+//const btnBackspace = document.querySelector('#backspace');
+const operands = document.querySelectorAll('.operand')
+const btnClear = document.querySelector('#btnClear');
+//const btnComa = document.querySelector('#btnComa');
+const equalsTo = document.querySelector('#btnEquals');
+
+for(let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', () => {
+        number += numbers[i].textContent;
+        screen.textContent = number
+    });
+};
+
+if (number == '22') console.log(number)
+
