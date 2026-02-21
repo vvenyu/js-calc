c2d0d562443195c56e73b04720e3ab732d4aec9e
wenyu <232058504+vvenyu@users.noreply.github.com>
2026-02-21 14:56:51 +0100

    Fixed bigs

--- script.js
+++ script.js
@@ -11,23 +11,30 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
-    return a / b;
+    if(b == 0) {
+        return 'Syntax Error';
+    } else return a / b;
 };
 
 let number = '';
 let operator;
 let secondNumber = '';
+let result = '';
 
 function operate(a, sign, b) {
+    //Changes a and b to be numbers instead of strings
+    let aNum = Number(a);
+    let bNum = Number(b);
+
     //Call one operator function
     if(sign === '+') {
-        return add(a, b);
+        return add(aNum, bNum);
     } else if (sign === '-') {
-        return subtract(a, b);
+        return subtract(aNum, bNum);
     } else if (sign === 'x') {
-        return multiply(a, b);
+        return multiply(aNum, bNum);
     } else if (sign === '÷') {
-        return divide(a, b);
+        return divide(aNum, bNum);
     } else return "ERROR";
 };
 
@@ -37,28 +44,57 @@ const numbers = document.querySelectorAll('.number');
 const operands = document.querySelectorAll('.operand')
 const btnClear = document.querySelector('#btnClear');
 //const btnComa = document.querySelector('#btnComa');
-const equalsTo = document.querySelector('#btnEquals');
 
 for(let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', () => {
         if(operator == undefined){
             number += numbers[i].textContent;
             screen.textContent = number;
+            console.log(`firstNumber: ${number}`);
         } else {
             secondNumber += numbers[i].textContent;
             screen.textContent = secondNumber;
-            console.log('hihihi');
-            console.log(operator)
+            console.log(`secondNumber ${secondNumber}`);
         }
     });
 };
 
 for(let j = 0; j < operands.length; j++) {
     operands[j].addEventListener('click', () => {
-        operator = operands[j].textContent;
+        if(operands[j].textContent !== '=') {
+            if(operator == undefined) {
+                operator = operands[j].textContent;
+                console.log(`operator: ${operator}`)
+            } else {
+                result = operate(number, operator, secondNumber);
+                operator = operands[j].textContent;
+                console.log(`secondOperator: ${operator}`);
+                secondNumber = '';
+                number = String(result);
+                screen.textContent = number;
+            }
+        } else {
+            if(operator == undefined) {
+                return
+            } else {
+                result = operate(number, operator, secondNumber);
+                operator = undefined;
+                secondNumber = '';
+                number = String(result)
+                screen.textContent = number;
+    
+                console.log(number);
+            };
+        };
     });
 };
 
-equalsTo.addEventListener('click', () => {
-    console.log(operate(number, operator, secondNumber));
-})
+
+//Clear's everything
+btnClear.addEventListener('click', () => {
+    result = '';
+    number = '';
+    operator = undefined;
+    secondNumber = '';
+    screen.textContent = '';
+});
